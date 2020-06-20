d944fdcf03b50362604c36d374c623cb836e4ecf
James Thompson <james@mavenlink.com>
2020-06-19 23:08:24 -0600

    Add basic help command to maven script.

---
+++ lib/help_command.rb
@@ -0,0 +1,15 @@
+class HelpCommand
+  def initialize(_options); end
+
+  def run
+    puts <<EOS
+
+The maven command is here to make your life at Mavenlink a little bit easier.
+
+For starters, it can help you get your laptop setup and ready to work on your
+first day. Over time we hope to add more helpful features to this tool. But,
+for now, it mainly helps you get a developer laptop setup quickly and easily.    
+
+EOS
+  end
+end
